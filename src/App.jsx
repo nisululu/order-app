0b8d6dc396e14d517a0694c1c5b577eafd98102a
N33XAL <nischaltandukar0@gmail.com>
2024-07-05 15:11:14 +0545

    implementing data loader and handling error with errorElement with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/cart/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       {
