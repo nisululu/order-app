1c1450852e030bde8a563634f5edbf6a7ecbf67e
N33XAL <nischaltandukar0@gmail.com>
2024-07-07 08:38:36 +0545

    Implemented Form action for new order

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/cart/CreateOrder";
+import CreateOrder, { orderAction } from "./features/cart/CreateOrder";
 import Order, { orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
@@ -21,7 +21,11 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: orderAction,
+      },
       {
         path: "/order/:orderId",
         element: <Order />,
